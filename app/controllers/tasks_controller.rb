a48ad65236e27424abe32657e84022fa8271aaa7
keita.aiba <keyfree.4322@gmail.com>
2020-05-17 13:54:26 +0000

    user6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy]
   
   def index
     @tasks = Task.all
@@ -61,7 +61,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to root_url
+      redirect_to root_path
     end
   end
 end
--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-Task.create(title: 'test title 1', content: 'test content 1')
-Task.create(title: 'test title 2', content: 'test content 2')
-Task.create(title: 'test title 3', content: 'test content 3')
-
-(1..100).each do |number|
-  Task.create(title: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
-end
